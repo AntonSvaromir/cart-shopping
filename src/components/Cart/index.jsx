fd5e95132c4b95eaf8097cbb1dd9d30f70d5bc1f
AntonSvaromir <tab42667@gmail.com>
2024-05-24 19:37:20 +0300

    add fetch and data

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,94 +1,119 @@
 import CartHeader from '../CartHeader'
 import Product from '../Product'
 import CartFooter from '../CartFooter'
-import data from '../../data'
 import { useEffect, useState } from 'react'
 
 function Cart() {
 	// Данные по товарам
-	const [cart, setCart] = useState(data)
+	const [cart, setCart] = useState(null)
 	// Общее количество выбранных товаров
-	const [total, setTotal] = useState({})
+	const [total, setTotal] = useState(null)
+	// Флаг для обновления запроса в базу
+	const [fetchData, setFetchData] = useState(true)
+
+	// Получение данных о товарах
+	useEffect(() => {
+		fetch('http://localhost:8000/products')
+		.then((res) => res.json())
+		.then((data) => setCart(data))
+	},[fetchData])
 	// Обновление количества выбранных товаров
 	useEffect(() => {
+		if (cart) {
 		setTotal({
 			price: cart.reduce((prev, curr) => prev + curr.priceTotal, 0),
 			count: cart.reduce((prev, curr) => prev + curr.count, 0),
 		})
+	}
 	}, [cart])
+
 	// Кнопка удалить
 	const deleteProduct = (id) => {
-		setCart((cart) => cart.filter((product) => id !== product.id))
+		// setCart((cart) => cart.filter((product) => id !== product.id))
+		fetch('http://localhost:8000/products/' + id, {
+			method: 'DELETE'
+		}).then((res) => {
+			res.ok && setFetchData((value) => !value)
+		})
 	}
 	// Увеличение количества товара
 	const increase = (id) => {
-		setCart((cart) => {
-			return cart.map((product) => {
-				if (product.id === id) {
-					return {
-						...product,
-						count: ++product.count,
-						priceTotal: product.count * product.price,
-					}
-				} else {
-					return product
-				}
-			})
+		// Ищем товар и изменяем 
+		const product = cart.find((product) => id === product.id)
+		const data = {
+			...product,
+			count: ++product.count,
+			priceTotal: product.count * product.price,
+		}
+		// Передаём на сервер изменённые данные 
+		fetch('http://localhost:8000/products/' + id, {
+			method: 'PUT',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(data),
+		}).then((res) => {
+			res.ok && setFetchData((value) => !value)
 		})
+		
 	}
-	// Уменьшение количества товара
-	const changeValue = (id, value) => {
-		setCart((cart) => {
-			return cart.map((product) => {
-				if (product.id === id) {
-					return {
-						...product,
-						count: value,
-						priceTotal: value * product.price,
-					}
-				} else {
-					return product
-				}
-			})
-		})
-	}
+	// Уменьшение количества товара	
 	const decrease = (id) => {
-		setCart((cart) => {
-			return cart.map((product) => {
-				if (product.id === id) {
-					return {
-						...product,
-						count: product.count > 1 ? --product.count : 1,
-						priceTotal: product.count * product.price,
-					}
-				} else {
-					return product
-				}
-			})
+		// Ищем товар и изменяем
+		const product = cart.find((product) => id === product.id)
+		const data = {
+			...product,
+			count: product.count > 1 ? --product.count : 1,
+			priceTotal: product.count * product.price,
+		}
+		// Передаём на сервер изменённые данные
+		fetch('http://localhost:8000/products/' + id, {
+			method: 'PUT',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(data),
+		}).then((res) => {
+			res.ok && setFetchData((value) => !value)
 		})
+		
+		
 	}
-
-	// Отрисовка карточек товара
-	const products = cart.map((product) => {
-		return (
-			<Product
-				key={product.id}
-				product={product}
-				deleteProduct={deleteProduct}
-				increase={increase}
-				decrease={decrease}
-				changeValue={changeValue}
-			/>
-		)
-	})
+	// Установка количества товара 
+	const changeValue = (id, value) => {
+		// Ищем товар и изменяем
+		const product = cart.find((product) => id === product.id)
+		const data = {
+			...product,
+			count: value,
+			priceTotal: value * product.price,
+		}
+		// Передаём на сервер изменённые данные
+		fetch('http://localhost:8000/products/' + id, {
+			method: 'PUT',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(data),
+		}).then((res) => {
+			res.ok && setFetchData((value) => !value)
+		})
+		
+	}
 
 	return (
 		<section className='cart'>
 			<CartHeader />
 
-			{products}
+			{cart &&
+				cart.map((product) => {
+					return (
+						<Product
+							key={product.id}
+							product={product}
+							deleteProduct={deleteProduct}
+							increase={increase}
+							decrease={decrease}
+							changeValue={changeValue}
+						/>
+					)
+				})}
 
-			<CartFooter total={total} />
+			{total && <CartFooter total={total} />}
 		</section>
 	)
 }
